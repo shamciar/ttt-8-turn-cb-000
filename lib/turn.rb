83e0bfe2e613d8cc53c2ccc92e9edd0b19b8e9cb
Jake Matteson <jake.matteson@gmail.com>
2017-10-04 21:08:00 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,3 +4,31 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def input_to_index(pos)
+  pos = pos.to_i
+  pos - 1
+end
+
+def position_taken?(board, index)
+  if board[index] == "X" || board[index] == "O"
+    !!true
+  else
+    !!false
+  end
+end
+
+def valid_move?(board, index)
+  if position_taken?(board, index)
+    !!false
+  elsif index < 0 || index > 8
+    !! false
+  else
+    !!true
+  end
+end
+
+def move(array, index, value = "X")
+  if valid_move?(array, index)
+    array[index] = value
+  end
+end
